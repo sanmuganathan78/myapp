87ebeb7a71d40b03e685286dc665037b6cf486c7
Boominathan <eboominathan@gmail.com>
2019-09-14 11:19:11 +0530

    Looping issue fixed

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,10 +1,12 @@
 export class Recipe {
     public name: string;
-    public desc: string;
+    public description: string;
     public imagePath: string;
-    constructor(name: string, desc: string, imagePath: string) {
-     this.name = name;
-        this.desc= desc;
-     this.imagePath = imagePath;   
+
+    constructor(name: string, description: string, imagePath: string) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+      
     }
 }
